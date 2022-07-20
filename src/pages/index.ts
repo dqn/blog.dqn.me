901fd954db48c9a8fe28b8a59e1c01433b079e18
dqn <dqn270@gmail.com>
2022-07-20 16:56:41 +0900

    feat: implement list page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,10 +1,79 @@
+import { readdir, readFile } from "fs/promises";
+import path from "path";
 import { E } from "../components/E.js";
 import { Layout } from "../components/Layout.js";
+import { entriesDir } from "../helpers/entriesDir.js";
+import { markdown } from "../parser/markdown/markdown.js";
 import { siteName } from "../siteName.js";
 import type { Component } from "../types/Component.js";
+import type { GetStaticProps } from "../types/GetStaticProps.js";
 
-const Top: Component = () => {
-  return Layout({ title: siteName }, E("h1", {}, "Hello World!"));
+type Entry = {
+  title: string;
+  date: string;
+  path: string;
+};
+
+type Props = {
+  entries: Entry[];
+};
+
+const Top: Component<Props> = ({ entries }) => {
+  return Layout(
+    { title: siteName },
+    E(
+      "ul",
+      { class: "space-y-4" },
+      ...entries.map(({ title, date, path }) =>
+        E(
+          "li",
+          {},
+          E(
+            "a",
+            { href: path, class: "text-white visited:text-visited" },
+            `${title} (${date})`
+          )
+        )
+      )
+    )
+  );
+};
+
+export const getStaticProps: GetStaticProps<
+  Props,
+  Record<string, never>
+> = async () => {
+  const paths = await readdir(entriesDir);
+
+  const entries = await Promise.all(
+    paths.map(async (p): Promise<Entry> => {
+      const fileContent = await readFile(path.join(entriesDir, p), "utf-8");
+      const result = markdown([...fileContent]);
+
+      if (!result.success) {
+        throw new Error("invalid markdown");
+      }
+
+      const { title, date } = result.data.frontmatter;
+
+      if (title === undefined || title === "") {
+        throw new Error("title is required");
+      }
+      if (date === undefined || date === "") {
+        throw new Error("date is required");
+      }
+
+      return {
+        title,
+        date,
+        path: path.join("/entries", p).replace(/.md$/, ""),
+      };
+    })
+  );
+
+  return {
+    entries: entries.sort((a, b) => b.date.localeCompare(a.date)),
+  };
 };
 
 export default Top;
