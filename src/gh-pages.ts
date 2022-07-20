b9e05385e1cc5da4a703da7a65a813b118196c5c
dqn <dqn270@gmail.com>
2022-07-21 05:19:00 +0900

    feat: add gh-pages script

---
+++ src/gh-pages.ts
@@ -0,0 +1,39 @@
+import { copyFile, mkdir, readdir, rm } from "node:fs/promises";
+import path, { dirname } from "node:path";
+
+const outDir = "out";
+const whitelist = [".git", outDir, "entries"];
+
+async function listFiles(dir: string): Promise<string[]> {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  const tasks = dirents.map(async (dirent) => {
+    const p = path.join(dir, dirent.name);
+    return dirent.isFile() ? p : await listFiles(p);
+  });
+
+  return await Promise.all(tasks).then((xs) => xs.flat());
+}
+
+async function main(): Promise<void> {
+  const contents = await readdir("./");
+  await Promise.all(
+    contents
+      .filter((x) => !whitelist.includes(x))
+      .map((x) => rm(x, { recursive: true, force: true }))
+  );
+  await listFiles(outDir).then((paths) =>
+    Promise.all(
+      paths.map(async (p) => {
+        const dest = p.replace(new RegExp(`^${outDir}/`), "");
+        console.log(dirname(dest));
+        if (dirname(dest) !== "") {
+          await mkdir(dirname(dest), { recursive: true });
+        }
+        return copyFile(p, dest);
+      })
+    )
+  );
+  await rm(outDir, { recursive: true, force: true });
+}
+
+await main();
